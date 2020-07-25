5eb04ef0d5caced39a77cba3b794556e1118e83e
Gobe Hobona <ghobona@opengeospatial.org>
2020-07-25 09:49:35 +0100

    updated OGC API - Features tutorial

--- replacestatic.py
+++ replacestatic.py
@@ -1,7 +1,10 @@
 import os, fnmatch
 # This file is for building the Github Pages (gh) site.
-# after building the project using make ogc, rename the build folder to docs and then run the file.
-# from https://stackoverflow.com/questions/4205854/python-way-to-recursively-find-and-replace-string-in-text-files
+# after building the project using make ogc, rename the build folder to docs,
+# then rename the _static folder to static,
+# then rename the _images folder to images,
+# then run the file.
+# From https://stackoverflow.com/questions/4205854/python-way-to-recursively-find-and-replace-string-in-text-files
 def findReplace(directory, find, replace, filePattern):
     for path, dirs, files in os.walk(os.path.abspath(directory)):
         for filename in fnmatch.filter(files, filePattern):
